dcfe2a89b130d0c30723721953ed7927fe2daf9a
Hameeda369 <hameedagm334@gmail.com>
2024-03-25 08:39:55 -0700

    push my guess number project

---
+++ app.js
@@ -0,0 +1,18 @@
+#! /usr/bun/env node
+console.log("guess a number between 1 to 10");
+// now variable create
+let a = Math.floor(Math.random() * 10);
+import inquirer from "inquirer";
+while (true) {
+    let input = await inquirer.prompt({ name: "guess", type: "number",
+        message: "Enter your guess number you want between 1 to 8:" });
+    // provide code
+    let ans = input.guess;
+    if (a == ans) {
+        console.log("congratulation your guess number absolutely correct");
+        break;
+    }
+    else {
+        console.log("Sorry you guess wrong number try again");
+    }
+}
